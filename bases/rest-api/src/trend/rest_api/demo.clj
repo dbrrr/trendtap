76c59a3fc17dbb5ab76fe0c9b8754347b529fee2
David Russell <david.russell@ardoq.com>
2024-10-19 20:46:05 +0200

    Clean up a bit

--- bases/rest-api/src/trend/rest_api/demo.clj
+++ bases/rest-api/src/trend/rest_api/demo.clj
@@ -1,6 +1,45 @@
 (ns trend.rest-api.demo
   (:require [trend.rest-api.common :as common]))
 
+(defn add-actor-row []
+  [:tr
+   [:td {:class "whitespace-nowrap py-2 pl-4 pr-3 text-sm sm:pl-0"}
+    [:div {:class "flex items-center"}
+     [:div {:class "relative"}
+      [:button {:type "button", :class "flex block w-full mt-2 rounded-lg border-2 border-dashed border-gray-300 p-2 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"}
+       [:svg {:xmlns "http://www.w3.org/2000/svg", :x "0px", :y "0px", :width "20", :height "20", :viewbox "0 0 16 16"}
+        [:circle {:cx "8", :cy "8", :r "8", :fill "#33cc33"}]
+        [:polygon {:fill "#fff", :points "12,7 9,7 9,4 7,4 7,7 4,7 4,9 7,9 7,12 9,12 9,9 12,9"}]]
+       [:span {:class "mx-2 block text-sm text-gray-900"} "Add coworker"]]]]]
+   [:td {:class "whitespace-nowrap px-3 py-2 text-sm text-gray-500"}]])
+
+(defn actor-row [{:keys [name current-user?]}]
+  (let [name-label (if current-user?
+                     "You"
+                     "Coworker")
+        content-label (if current-user?
+                        "What did you say?"
+                        "What did they say?")]
+    [:tr
+     [:td {:class "whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-0"}
+      [:div {:class "flex items-center"}
+       [:div {:class "relative"}
+        [:label {:for "name",
+                 :class "absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"} name-label]
+        [:input (merge
+                 {:type "text",
+                  :name "name",
+                  :id "name",
+                  :class "block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+                  :placeholder "Jane Smith"}
+                 (when current-user?
+                   {:disabled true
+                    :value name}))]]]]
+     [:td {:class "whitespace-nowrap px-3 py-4 text-sm text-gray-500"}
+      [:div {:class "relative"}
+       [:label {:for "name", :class "absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"} content-label]
+       [:textarea {:rows "2", :name "comment", :id "comment", :class "px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}]]]]))
+
 (defn silo []
   (common/render-and-respond
    [:html
@@ -25,57 +64,10 @@
            [:div {:class "inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8"}
             [:table {:class "min-w-full divide-y divide-gray-300"}
              [:tbody {:class "divide-y divide-gray-200 bg-white"}
-              [:tr
-               [:td {:class "whitespace-nowrap pt-2 pb-3 pl-4 pr-3 text-sm sm:pl-0"}
-                [:div {:class "flex items-center"}
-                 [:div {:class "relative"}
-                  [:label {:for "name", :class "absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"} "You"]
-                  [:input {:type "text",
-                           :name "name",
-                           :value "David Russell",
-                           :disabled true
-                           :id "name",
-                           :class "block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}]]]]
-               [:td {:class "whitespace-nowrap px-3 pb-3 pt-2 text-sm text-gray-500"}
-                [:div {:class "relative"}
-                 [:label {:for "name", :class "absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"} "What did you say?"]
-                 [:textarea {:rows "2", :name "comment", :id "comment", :class "px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}]
-                 ]]]
+              (actor-row {:name "David Russell" :current-user? true})
+              (actor-row {})
 
-              [:tr
-               [:td {:class "whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-0"}
-                [:div {:class "flex items-center"}
-                 [:div {:class "relative"}
-                  [:label {:for "name", :class "absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"} "Coworker"]
-                  [:input {:type "text", :name "name", :id "name", :class "block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6", :placeholder "Jane Smith"}]]
-                 ]]
-               [:td {:class "whitespace-nowrap px-3 py-4 text-sm text-gray-500"}
-                [:div {:class "relative"}
-                 [:label {:for "name", :class "absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"} "What did they say?"]
-                 [:textarea {:rows "2", :name "comment", :id "comment", :class "px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}]
-                 ]]]
-
-              [:tr
-                 [:td {:class "whitespace-nowrap py-2 pl-4 pr-3 text-sm sm:pl-0"}
-                  [:div {:class "flex items-center"}
-                   [:div {:class "relative"}
-                    [:button {:type "button", :class "flex block w-full mt-2 rounded-lg border-2 border-dashed border-gray-300 p-2 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"}
-                     [:svg {:xmlns "http://www.w3.org/2000/svg", :x "0px", :y "0px", :width "20", :height "20", :viewbox "0 0 16 16"}
-                      [:circle {:cx "8", :cy "8", :r "8", :fill "#33cc33"}]
-                      [:polygon {:fill "#fff", :points "12,7 9,7 9,4 7,4 7,7 4,7 4,9 7,9 7,12 9,12 9,9 12,9"}]]
-                     [:span {:class "mx-2 block text-sm text-gray-900"} "Add coworker"]]
-                   ]]
-                  ]
-               [:td {:class "whitespace-nowrap px-3 py-2 text-sm text-gray-500"}
-                ]
-
-               ]
-
-              ]]
-
-
-
-            ]]]]
+              (add-actor-row)]]]]]]
 
         [:div {:class "px-4 py-4 sm:px-6"}
          [:div {:class "flex justify-end"}
