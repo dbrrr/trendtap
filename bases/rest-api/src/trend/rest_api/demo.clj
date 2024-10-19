d21103c2d3fade9a0c0ea088b946c2f8426c9549
David Russell <david.russell@ardoq.com>
2024-10-19 21:03:53 +0200

    Data submitting appropriately

--- bases/rest-api/src/trend/rest_api/demo.clj
+++ bases/rest-api/src/trend/rest_api/demo.clj
@@ -17,13 +17,15 @@
        [:span {:class "mx-2 block text-sm text-gray-900"} "Add coworker"]]]]]
    [:td {:class "whitespace-nowrap px-3 py-2 text-sm text-gray-500"}]])
 
-(defn actor-row [{:keys [name current-user?]}]
+(defn actor-row [{:keys [id name current-user?]}]
   (let [name-label (if current-user?
                      "You"
                      "Coworker")
         content-label (if current-user?
                         "What did you say?"
-                        "What did they say?")]
+                        "What did they say?")
+        name-input-id (str "name-" id)
+        perspective-input-id (str "perspective-" id)]
     [:tr
      [:td {:class "whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-0"}
       [:div {:class "flex items-center"}
@@ -32,24 +34,32 @@
                  :class "absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"} name-label]
         [:input (merge
                  {:type "text",
-                  :name "name",
-                  :id "name",
+                  :name name-input-id,
+                  :id name-input-id
                   :class "block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
                   :placeholder "Jane Smith"}
                  (when current-user?
-                   {:disabled true
+                   {:readonly true
                     :value name}))]]]]
      [:td {:class "whitespace-nowrap px-3 py-4 text-sm text-gray-500"}
       [:div {:class "relative"}
        [:label {:for "name", :class "absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"} content-label]
-       [:textarea {:rows "2", :name "comment", :id "comment", :class "px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}]]]]))
+       [:textarea {:rows "2",
+                   :name perspective-input-id,
+                   :id perspective-input-id,
+                   :class "px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}]]]]))
+
+(defn- silo-description []
+  [:div {:class "mt-8"}
+   [:h2 {:class "text-l font-bold tracking-tight text-gray-900"} "How would you summarize your last meeting?"]
+   [:div {:class "mt-2"}
+    [:textarea {:rows "4", :name "description", :id "description", :class "px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}]]])
 
 (defn add-actor []
   (common/respond
    (str
     (common/render (actor-row {}))
-    (common/render (add-actor-row))))
-   )
+    (common/render (add-actor-row)))))
 
 (defn silo []
   (common/render-and-respond
@@ -64,10 +74,7 @@
           [:h2 {:class "text-3xl font-bold tracking-tight text-gray-900"} "Get started with an example"]
           [:p {:class "mt-3 text-md leading-6 text-gray-600"} "We're all about conversational intelligence. Briefly describe your last meeting, and we'll use it as an example."]]
 
-         [:div {:class "mt-8"}
-          [:h2 {:class "text-l font-bold tracking-tight text-gray-900"} "How would you summarize your last meeting?"]
-          [:div {:class "mt-2"}
-           [:textarea {:rows "4", :name "comment", :id "comment", :class "px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}]]]
+         (silo-description)
 
          [:div {:class "mt-8 flow-root"}
           [:h2 {:class "text-l font-bold tracking-tight text-gray-900"} "Who participated?"]
@@ -75,9 +82,10 @@
            [:div {:class "inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8"}
             [:table {:class "min-w-full divide-y divide-gray-300"}
              [:tbody {:class "divide-y divide-gray-200 bg-white"}
-              (actor-row {:name "David Russell" :current-user? true})
-              (actor-row {})
-
+              (actor-row {:id (random-uuid)
+                          :name "David Russell"
+                          :current-user? true})
+              (actor-row {:id (random-uuid)})
               (add-actor-row)]]]]]]
 
         [:div {:class "px-4 py-4 sm:px-6"}
